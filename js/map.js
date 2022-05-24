cd56dbd11830e9b583da27122ffdc9d9b0be9a8d
kosweb <kosweblink@gmail.com>
2022-05-24 13:53:21 +0300

    add map card, map pin renders

--- js/map.js
+++ js/map.js
@@ -1,23 +1,25 @@
 'use strict'
 
-const MIN_COORDI_ADDRESS = 50;
-const MAX_COORDI_ADDRESS = 1000;
 const MIN_PRICE = 1000;
 const MAX_PRICE = 100000;
 const MIN_ROOMS = 1;
 const MAX_ROOMS = 4;
 const MIN_GUESTS = 2;
 const MAX_GUESTS = 6;
-const MIX_X_COORDINATES = 50;
+const PIN_WIDTH = 44 / 2;
+const PIN_HEIGHT = 40 + 18;
+const MIN_X_COORDINATES = 50;
 const MAX_X_COORDINATES = 1150;
-const MIX_Y_COORDINATES = 130;
+const MIN_Y_COORDINATES = 130;
 const MAX_Y_COORDINATES = 630;
-const map = document.querySelector('.map').classList.remove('map--faded');
-const mapCardTemplate = document.querySelector('template').content.querySelector('.map__card');
+const map = document.querySelector('.map');
+map.classList.remove('map--faded');
+const mapFilterContainer = document.querySelector('.map__filters-container');
+const mapTemplate = document.querySelector('template').content.querySelector('.map__card');
 const mapPinTemplate = document.querySelector('template').content.querySelector('.map__pin');
 const mapPins = document.querySelector('.map__pins');
 
-var offers = [
+const offers = [
 	'Большая уютная квартира',
 	'Маленькая неуютная квартира',
 	'Огромный прекрасный дворец',
@@ -28,12 +30,12 @@ var offers = [
 	'Неуютное бунгало по колено в воде'
 ];
 
-var houseTypes = ['palace', 'flat', 'house', 'bungalo'];
-var checkTimes = ['12:00', '13:00', '14:00'];
-var featuresArr = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var photoArr = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const houseTypes = ['palace', 'flat', 'house', 'bungalo'];
+const checkTimes = ['12:00', '13:00', '14:00'];
+const featuresArr = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const photoArr = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-function getRandomInteger(min, max) {
+function getRandomInt(min, max) {
   let rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
@@ -54,25 +56,29 @@ const getMapOffers = () => {
 	let newArr = [];
 
 	for (let i = 0; i < 7; i++) {
+
+		let getXcoordinate = getRandomInt(MIN_X_COORDINATES, MAX_X_COORDINATES) + PIN_WIDTH;
+		let getYcoordinate = getRandomInt(MIN_Y_COORDINATES, MAX_Y_COORDINATES) + PIN_HEIGHT;
+
 		let newObj = {
 			author: {
 				avatar: `img/avatars/user0${i+1}.png`
 			},
 			offer: {
 				title: offers[i],
-				address: `${getRandomInteger(MIN_COORDI_ADDRESS, MAX_COORDI_ADDRESS)}, ${getRandomInteger(MIN_COORDI_ADDRESS, MAX_COORDI_ADDRESS)}`,
-				price: getRandomInteger(MIN_PRICE, MAX_PRICE),
+				address: `${getXcoordinate}, ${getYcoordinate}`,
+				price: getRandomInt(MIN_PRICE, MAX_PRICE),
 				type: getRandomIndex(houseTypes),
-				rooms: getRandomInteger(MIN_ROOMS, MAX_ROOMS),
-				guests: getRandomInteger(MIN_GUESTS, MAX_GUESTS),
+				rooms: getRandomInt(MIN_ROOMS, MAX_ROOMS),
+				guests: getRandomInt(MIN_GUESTS, MAX_GUESTS),
 				checkin: getRandomIndex(checkTimes),
 				checkout: getRandomIndex(checkTimes),
-				features: featuresArr.slice(getRandomInteger(0, 2), getrandomInteger(3, 6)),
+				features: featuresArr.slice(getRandomInt(0, 2), getRandomInt(3, 6)),
 				description: '',
 				photos: shuffle(photoArr),
 				location: {
-					x: getRandomInteger(MIX_X_COORDINATES, MAX_X_COORDINATES),
-					y: getRandomInteger(MIX_Y_COORDINATES, MAX_Y_COORDINATES)
+					x: getXcoordinate,
+					y: getYcoordinate
 				}
 			}
 		}
@@ -84,19 +90,66 @@ const getMapOffers = () => {
 
 const mapOffers = getMapOffers();
 
-const renderMapPin = (offers) => {
+const renderMapCard = (arr) => {
+	const mapCard = mapTemplate.cloneNode('true');
+	const featuresBox = mapCard.querySelector('.popup__features');
+	const features = mapCard.querySelectorAll('.feature');
+
+	mapCard.querySelector('.map__card-avatar').src = arr.author.avatar;
+	mapCard.querySelector('.popup__title').textContent = arr.offer.title;
+	mapCard.querySelector('.popup__text--address').textContent = arr.offer.address;
+	mapCard.querySelector('.popup__text--price').textContent = arr.offer.price;
+	mapCard.querySelector('.popup__type').textContent = arr.offer.type;
+	mapCard.querySelector('.popup__text--capacity').textContent = `${arr.offer.rooms} комнаты для ${arr.offer.guests} гостей`;
+	mapCard.querySelector('.popup__text--time').textContent = `Заезд после ${arr.offer.checkin}, выезд до ${arr.offer.checkout}`;
+
+
+	features.forEach((feature, i) => {
+		if (feature.dataset.set !== arr.offer.features[i]) {
+			featuresBox.removeChild(feature);
+		}
+	});
+
+	mapCard.querySelector('.popup__description').textContent = arr.offer.description;
+
+	const cardPictures = mapCard.querySelector('.popup__photos');
+	mapCard.querySelector('.popup__photos--img').src = arr.offer.photos[0];
+
+	for (let i = 0; i < arr.offer.photos.length - 1; i++) {
+		const cardItemCopy = mapCard.querySelector('.popup__photos--item').cloneNode('true');
+		cardItemCopy.querySelector('.popup__photos--img').src = arr.offer.photos[i + 1];
+		cardPictures.appendChild(cardItemCopy);
+	};
+
+  return mapCard;
+};
+
+const renderMapPin = (arr) => {
 	const mapPin = mapPinTemplate.cloneNode('true');
 
-  mapPin.style = `left: ${offers.offer.location.x}px; top: ${offers.offer.location.y}px;`;
-  mapPin.querySelector('.map__pin-avatar').src = offers.author.avatar;
-  mapPin.querySelector('.map__pin-avatar').alt = offers.offer.title;
+  mapPin.style = `left: ${arr.offer.location.x}px; top: ${arr.offer.location.y}px;`;
+  mapPin.querySelector('.map__pin-avatar').src = arr.author.avatar;
+  mapPin.querySelector('.map__pin-avatar').alt = arr.offer.title;
 
   return mapPin;
 };
 
-var fragment = document.createDocumentFragment();
-for (let i = 0; i < mapOffers.length; i++) {
-	fragment.appendChild(renderMapPin(mapOffers[i]));
-};
 
-mapPins.appendChild(fragment);
+const appendMapPin = () => {
+	const fragment = document.createDocumentFragment();
+
+	for (let i = 0; i < mapOffers.length; i++) {
+		fragment.appendChild(renderMapPin(mapOffers[i]));
+	};
+
+	return mapPins.appendChild(fragment);
+}
+
+const appendMapCard = () => {
+	const fragment = document.createDocumentFragment();
+	fragment.appendChild(renderMapCard(mapOffers[0]));
+	return map.appendChild(fragment);
+}
+
+appendMapPin();
+appendMapCard();
